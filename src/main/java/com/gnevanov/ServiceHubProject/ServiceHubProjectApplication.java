cbc0c282e398641974564c990be361c93678ac92
Egor <gnevanov.egor@gmail.com>
2020-09-01 00:02:13 +0300

    Initial commit

---
+++ src/main/java/com/gnevanov/ServiceHubProject/ServiceHubProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gnevanov.ServiceHubProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceHubProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceHubProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gnevanov/ServiceHubProject/ServiceHubProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gnevanov.ServiceHubProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceHubProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
